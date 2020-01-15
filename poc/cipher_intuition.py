39b6e15c289ea9a35374eebfd2ff847a13551c9b
weghstee <guillaume.weghsteen@eurecom.fr>
2020-01-15 17:49:37 +0100

    add cypher intuition (alg from 5.1)

---
+++ poc/cipher_intuition.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class CypherIntuition:
+    """Defines the crypto operations explained in section 5.1"""
+    def __init__(self, p: int):
+        """Initialize the instance for cypher operations.
+
+        Args:
+            p: modulo prime for Zp
+        """
+        self.p = p
+
+    def noisy_enc(self, rho, x, r):
+        """Encrypt the noised value using the 0-share.
+        Args:
+            rho: 0-share specific to the participant
+            x: real value of the participant
+            r: noise to add to the value for privacy preservation
+        """
+        return((x + r + rho) % self.p)
+
+    def AggrDec(self, aggregator_rho, c: List[int]):
+        """The decryption algorithm for the aggregator.
+        Args:
+            aggregator_rho: the zero share of the aggregator
+            c: list of cyphertexts (noisy encrypted)
+        """
+        return(aggregator_rho + sum(c))
+
+    def randomize(self, x: int, r: int):
+        """Khi function. Incorporate additive noise for encrypting the data."""
+        return (x + r) % self.p
---
+++ poc/tests/test_cypher_intuition.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest import TestCase
+from ..cypher_intuition import CypherIntuition
+
+
+class Test_CypherIntuition(TestCase):
+    def setUp(self):
+        DATA_MAX_BOUND = 2000  # delta
+        N = 20  # number of participants
+        P = 32323  # P as in Zp (prime number)
+
+        self.cypher = CypherIntuition(P)
+
+    def test
+
+if __name__ == '__main__':
+    unittest.main()
