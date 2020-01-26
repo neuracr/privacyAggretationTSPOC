c8f9ffdcd635ce3fd450b195e6119d917a3f17e7
weghstee <guillaume.weghsteen@eurecom.fr>
2020-01-26 18:53:58 +0100

    clean code for linter and presentation

--- poc/plotter.py
+++ poc/plotter.py
@@ -1,15 +1,13 @@
-from random import random
 from math import exp
 import logging
 
-from bokeh.layouts import column, layout
-from bokeh.models import Button, Plot
-from bokeh.palettes import RdYlBu3
+from bokeh.layouts import layout
+from bokeh.models import Button
 from bokeh.plotting import figure, curdoc
 from bokeh.models.glyphs import VBar
-from bokeh.models import ColumnDataSource, TextInput, FactorRange, Slider, Div, Paragraph, Whisker
+from bokeh.models import (ColumnDataSource, TextInput, Slider, Div, Paragraph,
+                          Whisker)
 from bokeh.models import Toggle
-from bokeh.transform import factor_cmap
 from os.path import dirname, join
 from bokeh.driving import linear
 
@@ -22,13 +20,17 @@ number_participants = [10, 100, 1000, 10000]
 
 # Widget variables (parameters for the simulation):
 eps = TextInput(title="ε", value=str(0.1))
-eps_slider = Slider(title="ε", value=float(eps.value), start=0, end=2, step=0.1)
+eps_slider = Slider(title="ε", value=float(eps.value), start=0, end=2,
+                    step=0.1)
 small_delta = TextInput(title="δ", value=str(0.01))
-small_delta_slider = Slider(title="δ", value=float(small_delta.value), start=0, end=2, step=0.01)
+small_delta_slider = Slider(title="δ", value=float(small_delta.value), start=0,
+                            end=2, step=0.01)
 big_delta = TextInput(title="Δ", value=str(87))
-big_delta_slider = Slider(title="Δ", value=float(big_delta.value), start=1, end=2000, step=1)
+big_delta_slider = Slider(title="Δ", value=float(big_delta.value), start=1,
+                          end=2000, step=1)
 gamma = TextInput(title="γ", value=str(0.2))
-gamma_slider = Slider(title="γ", value=float(gamma.value), start=0, end=1, step=0.1)
+gamma_slider = Slider(title="γ", value=float(gamma.value), start=0, end=1,
+                      step=0.1)
 
 # result of the standard deviation of the symmetric geometric law for the noise
 geo_text = Paragraph(text="")
@@ -37,12 +39,16 @@ geo_text = Paragraph(text="")
 enable_1exp_btn = Toggle(active=True, label="Enable simulation")
 enable_refine_btn = Toggle(active=False, label="Enable error stat refining")
 err_percent_btn = Toggle(active=False, label="Computes all errors in percent")
-# tracks if one of the parameter changed to know if we start the stats from 0 again
+redo_1exp_btn = Button(label="Redo experiment")
+
+# tracks if one of the parameter changed to know if we start the stats from
+# 0 again
 has_param_changed = True
 
 source_1exp = ColumnDataSource(data=dict(n=[], error=[]))
 
-p_1exp = figure(plot_height=350, title="1 time error", x_axis_type="log", y_range=[0, 100], tools="ypan, ywheel_pan, ywheel_zoom")  # y_axis_type="log",
+p_1exp = figure(plot_height=350, title="1 time error", x_axis_type="log",
+                y_range=[0, 100], tools="ypan, ywheel_pan, ywheel_zoom")
 glyph = VBar(x='n', top='error', bottom=0.1, width="n", hatch_scale=None)
 p_1exp.add_glyph(source_1exp, glyph)
 p_1exp.y_range.start = 0
@@ -65,7 +71,8 @@ def update_1exp():
     # https://www.wolframalpha.com/input/?i=%E2%88%91a%5E%28-i%29*i%5E2
     alpha = exp(eps_val/big_delta_val)
     geo_sigma = (2*alpha/((alpha-1)**2))**0.5
-    geo_text.text = 'Standard deviation of the added noise σ(Geom(α)) = %d' % (geo_sigma)
+    geo_text.text = ('Standard deviation of the added noise σ(Geom(α)) = %d'
+                     % (geo_sigma))
 
     if not enable_1exp_btn.active:
         return
@@ -111,12 +118,19 @@ def on_slider_change():
 
 
 # data for the empirical study of the error
-source_stat = ColumnDataSource(data=dict(n_participants=[], n_iterations=[], mean_error=[], std_deviation=[], upper=[], lower=[]))
+source_stat = ColumnDataSource(data=dict(n_participants=[], n_iterations=[],
+                               mean_error=[], std_deviation=[], upper=[],
+                               lower=[]))
 
-p_error_stat = figure(plot_height=350, title="empirical mean, std deviation error", x_axis_type="log", y_range=[0, 100], tools="ypan, ywheel_pan, ywheel_zoom")  # y_axis_type="log",
+p_error_stat = figure(plot_height=350,
+                      title="empirical mean, std deviation error",
+                      x_axis_type="log",
+                      y_range=[0, 100],
+                      tools="ypan, ywheel_pan, ywheel_zoom")
 
 # mean error bar
-glyph = VBar(x='n_participants', top='mean_error', bottom=0.1, width="n_participants", hatch_scale=None)
+glyph = VBar(x='n_participants', top='mean_error', bottom=0.1,
+             width="n_participants", hatch_scale=None)
 p_error_stat.add_glyph(source_stat, glyph)
 p_error_stat.y_range.start = 0
 p_error_stat.x_range.range_padding = 0.1
@@ -125,13 +139,15 @@ p_error_stat.xgrid.grid_line_color = None
 
 # overlay for the standard deviation
 p_error_stat.add_layout(
-    Whisker(source=source_stat, base="n_participants", upper="upper", lower="lower", level="overlay")
+    Whisker(source=source_stat, base="n_participants", upper="upper",
+            lower="lower", level="overlay")
 )
 
 
 @linear()
 def refine_stat(step):
-    """refine the experimental computation of the mean and deviation of the error"""
+    """Refine the experimental computation of the mean and deviation of
+       the error"""
     sanitize_params()
     eps_val = float(eps.value)
     small_delta_val = float(small_delta.value)
@@ -142,28 +158,43 @@ def refine_stat(step):
         return
 
     global has_param_changed
-    # if the parameters of the simulation have changed, we need to recompute everything from scratch
+    # if the parameters of the simulation have changed, we need to recompute
+    # everything from scratch
     if has_param_changed:
         n = len(number_participants)
-        source_stat.data = dict(n_participants=number_participants, n_iterations=[0]*n, mean_error=[0]*n, std_deviation=[0]*n, upper=[0]*n, lower=[0]*n)
+        source_stat.data = dict(n_participants=number_participants,
+                                n_iterations=[0]*n,
+                                mean_error=[0]*n,
+                                std_deviation=[0]*n,
+                                upper=[0]*n,
+                                lower=[0]*n)
         has_param_changed = False
 
-    new_data = dict(n_participants=[], n_iterations=[], mean_error=[], std_deviation=[], upper=[], lower=[])
+    new_data = dict(n_participants=[], n_iterations=[], mean_error=[],
+                    std_deviation=[], upper=[], lower=[])
 
     for i in range(len(number_participants)):
-        res, real, p = experiment_basic(number_participants[i], 1337, eps_val, small_delta_val,
-                                        big_delta_val, gamma_val)
+        res, real, p = experiment_basic(number_participants[i], 1337, eps_val,
+                                        small_delta_val, big_delta_val,
+                                        gamma_val)
         err = modular_abs(real, res, p)
         if err_percent_btn.active:
             err = err/real*100
 
         current_iteration_nb = source_stat.data['n_iterations'][i]+1
         new_data['n_participants'] += [number_participants[i]]
-        new_data['mean_error'] += [(source_stat.data['mean_error'][i])*(current_iteration_nb-1)/(current_iteration_nb) + err/current_iteration_nb]
-        new_data['std_deviation'] += [(source_stat.data['std_deviation'][i]**2 * (current_iteration_nb-1)/current_iteration_nb +
-                                        err**2 / current_iteration_nb)**0.5]
-        new_data['upper'] += [new_data['mean_error'][-1] + new_data['std_deviation'][-1]]
-        new_data['lower'] += [new_data['mean_error'][-1] - new_data['std_deviation'][-1]]
+        new_data['mean_error'] += [(source_stat.data['mean_error'][i])
+                                   * (current_iteration_nb-1)
+                                   / (current_iteration_nb)
+                                   + err / current_iteration_nb]
+        new_data['std_deviation'] += [(source_stat.data['std_deviation'][i]**2
+                                      * (current_iteration_nb-1)
+                                      / current_iteration_nb
+                                      + err**2 / current_iteration_nb)**0.5]
+        new_data['upper'] += [new_data['mean_error'][-1]
+                              + new_data['std_deviation'][-1]]
+        new_data['lower'] += [new_data['mean_error'][-1]
+                              - new_data['std_deviation'][-1]]
         new_data['n_iterations'] += [current_iteration_nb]
     source_stat.data = new_data
     for x in source_stat.data.keys():
@@ -186,16 +217,19 @@ def sanitize_params():
     gamma_slider.value = float(gamma_slider.value)
 
 
-desc = Div(text=open(join(dirname(__file__), "error_plot.html")).read(), sizing_mode="stretch_width")
+desc = Div(text=open(join(dirname(__file__), "error_plot.html")).read(),
+           sizing_mode="stretch_width")
 
-l = layout([
+lay = layout([
     [desc],
     [eps, eps_slider],
     [small_delta, small_delta_slider],
     [big_delta, big_delta_slider],
     [gamma, gamma_slider],
-    [err_percent_btn, enable_1exp_btn, geo_text, enable_refine_btn],
-    [p_1exp, p_error_stat]
+    [geo_text],
+    [err_percent_btn, enable_1exp_btn, enable_refine_btn],
+    [p_1exp, p_error_stat],
+    [redo_1exp_btn]
 ])
 
 controls = [eps, small_delta, big_delta, gamma]
@@ -203,6 +237,8 @@ for control in controls:
     control.on_change('value', lambda attr, old, new: update_1exp())
 
 err_percent_btn.on_change('active', on_err_percent_btn_changed)
+
+redo_1exp_btn.on_click(update_1exp)
 
 enable_1exp_btn.on_change('active', lambda attr, old, new: update_1exp())
 sliders = [eps_slider, small_delta_slider, big_delta_slider, gamma_slider]
@@ -214,9 +250,8 @@ for ti in textinputs:
     ti.on_change('value', lambda attr, old, new: on_textinput_change())
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(l)
+curdoc().add_root(lay)
 curdoc().title = "Empirical error"
 update_1exp()
 curdoc().add_periodic_callback(refine_stat, 500)
 logging.basicConfig(level=logging.DEBUG)
-
